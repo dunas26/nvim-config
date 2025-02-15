9d8cf45f2a6cfa7d8b034a14194f977a63617387
dunas26 <jhonjamesillo@gmail.com>
2025-02-15 13:18:38 -0500

    fix(tabby): error opening buffer tabs

--- lua/dunas26/plugins/tabby.lua
+++ lua/dunas26/plugins/tabby.lua
@@ -2,6 +2,9 @@ return {
 	"nanozuki/tabby.nvim",
 	event = "VimEnter",
 	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require"tabby".setup{}
+	end,
 	opts = {
 		preset = "active_wins_at_tail",
 		option = {

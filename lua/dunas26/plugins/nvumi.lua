d25b3fed6a2d5e5933b93957cf861ff95f934e9f
dunas26 <jhonjamesillo@gmail.com>
2025-03-01 21:51:18 -0500

    feat(nvumi): new plugin

---
+++ lua/dunas26/plugins/nvumi.lua
@@ -0,0 +1,18 @@
+return {
+  "josephburgess/nvumi",
+  dependencies = { "folke/snacks.nvim" },
+  opts = {
+    virtual_text = "newline", -- or "inline"
+    prefix = " 🚀 ", -- prefix shown before the output
+    date_format = "iso", -- or: "uk", "us", "long"
+    keys = {
+      run = "<CR>", -- run/refresh calculations
+      reset = "R", -- reset buffer
+      yank = "<leader>y", -- yank output of current line
+      yank_all = "<leader>Y", -- yank all outputs
+    },
+    custom_conversions = {
+      -- see section below
+    }
+  }
+}
